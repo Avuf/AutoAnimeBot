ad63d67b55c63becb602b34664e49c2ab9882a9d
shinigamiezz <168072718+shinigamiezz@users.noreply.github.com>
2024-06-23 17:11:42 +0530

    Update config.py

--- functions/config.py
+++ functions/config.py
@@ -22,29 +22,28 @@ from decouple import config
 class Var:
     # Telegram Credentials
 
-    API_ID = int(23363032)
-    API_HASH = "74134aaa77f0d0725385377e104bf933"
-    BOT_TOKEN = "6527490872:AAGneA1tbChM4JtOAhgzTg4uQ59HmBgTqKk"
+    API_ID = int(22413676)
+    API_HASH = "654970bafe567f9d618a257e93660551"
+    BOT_TOKEN = "7151855821:AAGHjXGoGael0vvM2sRgMXktX_zWtN4uedw"
     SESSION = ""
     WEB = False
     # Database Credentials
 
     FIREBASE_URL = "https://auto-anime-as-default-rtdb.firebaseio.com"
-    FIREBASE_SERVICE_ACCOUNT_FILE = "https://gist.githubusercontent.com/shinigamiezz/0444921a49fb188ee2205a983317ebff/raw/0bb8adcc4035dd54149ca1fcdcb9eaeb17eb9a55/service.json"
-
+    FIREBASE_SERVICE_ACCOUNT_FILE = "https://gist.githubusercontent.com/shinigamiezz/0444921a49fb188ee2205a983317ebff/raw/0bb8adcc4035dd54149ca1fcdcb9eaeb17eb9a55/service.json
     MDB_URI = config("MDB_URI", default="mongodb+srv://Emiliatg:Emiliatg@cluster0.c0a2uor.mongodb.net/?retryWrites=true&w=majority")
     MDB_NAME = config("MDB_NAME", default="Cluster0")
     
     
     # Channels Ids
 
-    BACKUP_CHANNEL = config("BACKUP_CHANNEL", default=0, cast=int)
-    MAIN_CHANNEL = config("MAIN_CHANNEL", default=0, cast=int)
-    LOG_CHANNEL = int(-1002219868353)
-    CLOUD_CHANNEL = config("CLOUD_CHANNEL", default=0, cast=int)
-    OWNER = config("OWNER", default=0, cast=int)
-    FORCESUB_CHANNEL1 =config("FORCESUB_CHANNEL1", default=0, cast=int)
-    FORCESUB_CHANNEL2 =config("FORCESUB_CHANNEL2", default=0, cast=int)
+    BACKUP_CHANNEL = config("-1002168406189", default=0, cast=int)
+    MAIN_CHANNEL = config("-1002177615094", default=0, cast=int)
+    LOG_CHANNEL = int(-1002247750933)
+    CLOUD_CHANNEL = config("-1002204000606", default=0, cast=int)
+    OWNER = config("6052897917", default=0, cast=int)
+    FORCESUB_CHANNEL1 =config("-1002177615094", default=0, cast=int)
+    FORCESUB_CHANNEL2 =config("-1002168406189", default=0, cast=int)
 
     LINK1 = None
     LINK2 = None
