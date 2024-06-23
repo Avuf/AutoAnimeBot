5bab414640b330efa1f54cf9b31e339a3fe536e6
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2024-06-23 12:24:47 +0530

    Update __init__.py

--- functions/__init__.py
+++ functions/__init__.py
@@ -7,7 +7,7 @@ routes = web.RouteTableDef()
 async def root_route_handler(request):
     return web.json_response("Vysakh")
 
-async def web_server():
+def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
     return web_app
